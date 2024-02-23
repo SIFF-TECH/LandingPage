777f4aa98a82b034c10b9edebe55b8386c755891
BENAMOUD YOUCEF SEIF EDDINE <youcef.benamoud@univ-constantine2.dz>
2024-02-23 22:42:20 +0100

    landingpage with java script

---
+++ javascript/landingpage/pep/pep.js
@@ -0,0 +1,9 @@
+function changecolor(color) {
+    const img =document.querySelector('.sec');
+    img.style.background =color;
+    
+}
+function changeimg(something) {
+    document.querySelector('.imgbox-js').src=something;
+    
+}
